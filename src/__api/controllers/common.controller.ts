c2892e3c63875e92fad532054eeceaa623dfa0c8
ehddk <bj9147@naver.com>
2024-11-12 22:20:40 +0900

    최근사항 반영

--- src/__api/controllers/common.controller.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export const healthCheck = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  res.send("서버 건강하다잉");
-};
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-// index.ts
-
-import "@/envs";
-
-import "@/db/mongoose";
-
-import "@/app";
